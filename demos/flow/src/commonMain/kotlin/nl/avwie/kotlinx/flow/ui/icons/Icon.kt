02a7fb3a39576be40086998666daa84a6f9bdc3f
Arjan van Wieringen <avanwieringen@Arjans-Air.home>
2023-04-27 11:25:39 +0200

    Improved icons

--- demos/flow/src/commonMain/kotlin/nl/avwie/kotlinx/flow/ui/icons/Icon.kt
+++ demos/flow/src/commonMain/kotlin/nl/avwie/kotlinx/flow/ui/icons/Icon.kt
@@ -3,10 +3,14 @@ package nl.avwie.kotlinx.flow.ui.icons
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.gestures.onDrag
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.onClick
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
@@ -17,6 +21,8 @@ import androidx.compose.ui.graphics.drawscope.DrawStyle
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.*
 import androidx.compose.ui.zIndex
 import nl.avwie.kotlinx.flow.state.IconState
@@ -34,32 +40,47 @@ import nl.avwie.kotlinx.utils.compose.toIntOffset
 ) {
     val density = LocalDensity.current
 
-    Canvas(
+    Box(
         modifier = Modifier
-            .size(width = iconState.type.size.width, height = iconState.type.size.height)
             .offset { toIntOffset(iconState.position) }
-            .onClick { onClick() }
-            .onDrag { offset -> onDrag(density.toDpOffset(offset)) }
-            .zIndex(if (iconState.selected) 1f else 0f)
     ) {
-        when (iconState.type) {
-            IconType.Start -> StartIcon(iconState)
-            IconType.Action -> ActionIcon(iconState)
-            IconType.End -> EndIcon(iconState)
+        Box(
+            contentAlignment = Alignment.Center,
+        ) {
+            Canvas(
+                modifier = Modifier
+                    .size(width = iconState.type.size.width, height = iconState.type.size.height)
+                    .onClick { onClick() }
+                    .onDrag { offset -> onDrag(density.toDpOffset(offset)) }
+            ) {
+                when (iconState.type) {
+                    IconType.Start -> StartIcon(iconState)
+                    IconType.Action -> ActionIcon(iconState)
+                    IconType.End -> EndIcon(iconState)
+                }
+            }
+
+            BasicText(
+                text = iconState.name,
+                style = TextStyle(
+                    fontSize = 11.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
         }
-    }
 
-    if (iconState.selected) {
-        val selectorRect = DpRect(
-            origin = iconState.position,
-            size = iconState.type.size
-        ).grow(4.dp)
+        if (iconState.selected) {
+            val selectorRect = DpRect(
+                origin = iconState.position,
+                size = iconState.type.size
+            ).grow(4.dp)
 
-        SelectionBox(
-            modifier = Modifier
-                .size(selectorRect.size)
-                .offset { IntOffset(x = selectorRect.left.toPx().toInt(), y = selectorRect.top.toPx().toInt()) }
-        )
+            SelectionBox(
+                modifier = Modifier
+                    .size(selectorRect.size)
+                    .offset { IntOffset(x = -4.dp.toPx().toInt(), y = -4.dp.toPx().toInt()) }
+            )
+        }
     }
 }
 
--- demos/flow/src/jvmMain/kotlin/nl/avwie/kotlinx/flow/FlowDemo.kt
+++ demos/flow/src/jvmMain/kotlin/nl/avwie/kotlinx/flow/FlowDemo.kt
@@ -14,9 +14,9 @@ fun main() {
         Flow(
             storeModule = StoreModule(
                 initialIconStates = listOf(
-                    IconState(uuid4(), "Foo", DpOffset(100.dp, 100.dp), IconType.Start, selected = true),
-                    IconState(uuid4(), "Bar", DpOffset(200.dp, 200.dp), IconType.Action),
-                    IconState(uuid4(), "Baz", DpOffset(300.dp, 300.dp), IconType.End),
+                    IconState(uuid4(), "Start", DpOffset(100.dp, 100.dp), IconType.Start, selected = true),
+                    IconState(uuid4(), "Some activity", DpOffset(200.dp, 200.dp), IconType.Action),
+                    IconState(uuid4(), "End", DpOffset(300.dp, 300.dp), IconType.End),
                 )
             ),
         )
